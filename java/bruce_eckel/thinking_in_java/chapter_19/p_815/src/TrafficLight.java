969d508b4153c0de72e1deb109cab2b7896c8c02
stache1draht <stacheldraht23@gmail.com>
2020-08-23 20:27:49 +0300

    no message

---
+++ java/bruce_eckel/thinking_in_java/chapter_19/p_815/src/TrafficLight.java
@@ -0,0 +1,34 @@
+/** This code sample demonstrates using enums with the switch operator **/
+
+enum Signal {GREEN, YELLOW, RED}
+
+public class TrafficLight {
+    Signal color=Signal.RED;
+
+    public void change(){
+        switch (color){
+            case RED: color=Signal.GREEN;
+            break;
+
+            case GREEN: color=Signal.YELLOW;
+            break;
+
+            case YELLOW: color=Signal.RED;
+            }
+        }
+
+    public String toString() {
+        return "The traffic light is " + color;
+        }
+
+        public static void main(String[] args) {
+            TrafficLight t=new TrafficLight();
+
+            for (int i=0; i<7; i++){
+                System.out.println(t);
+                t.change();
+            }
+    }
+
+}
+
