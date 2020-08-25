0e7941d33268dbded12afa0748ccaaa920ef51ee
stache1draht <stacheldraht23@gmail.com>
2020-08-25 23:08:10 +0300

    no message

---
+++ java/bruce_eckel/thinking_in_java/chapter_19/p_818/src/UpcastEnum.java
@@ -0,0 +1,11 @@
+enum Search{HITHER, YON}
+
+public class UpcastEnum {
+    public static void main(String[] args){
+        Search[] vals=Search.values();
+        Enum e=Search.HITHER;
+
+        for(Enum en:e.getClass().getEnumConstants())
+            System.out.println(en);
+    }
+}
