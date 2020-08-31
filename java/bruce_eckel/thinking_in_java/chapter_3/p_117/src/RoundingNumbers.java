32b7e22bcc51512769da514757d4d03508ac6920
stache1draht <stacheldraht23@gmail.com>
2020-08-31 23:01:33 +0300

    no message

---
+++ java/bruce_eckel/thinking_in_java/chapter_3/p_117/src/RoundingNumbers.java
@@ -0,0 +1,12 @@
+public class RoundingNumbers {
+    public static void main(String[] args) {
+        double above=0.7, below=0.4;
+        float fabove=0.7f, fbelow=0.4f;
+
+        System.out.println("Math.round (above): "+Math.round(above));
+        System.out.println("Math.round (above): "+Math.round(below));
+        System.out.println("Math.round (above): "+Math.round(fabove));
+        System.out.println("Math.round (above): "+Math.round(fbelow));
+
+    }
+}
