80e289c6247056467588099dc105d82839e0e90a
stache1draht <stacheldraht23@gmail.com>
2020-09-05 23:30:22 +0300

    no message

---
+++ java/bruce_eckel/thinking_in_java/chapter_7/p_227/src/BlankFinal.java
@@ -0,0 +1,29 @@
+class Poppet{
+    private int i;
+    Poppet(int i){
+        i=11;
+    }
+}
+
+public class BlankFinal {
+    private final int i=0;
+
+    //blank constants must be initialized in the constructor
+    private final int j;
+    private final Poppet p;
+
+    public BlankFinal(){
+        j=1;
+        p=new Poppet(1);
+    }
+
+    public BlankFinal(int x){
+        j=x;
+        p=new Poppet(x);
+    }
+
+    public static void main(String[] args){
+        new BlankFinal();
+        new BlankFinal(47);
+    }
+}
