e27825272efe1f4a874f27feed4f17e7b5338507
stache1draht <stacheldraht23@gmail.com>
2020-09-15 23:12:06 +0300

    no message

---
+++ java/bruce_eckel/thinking_in_java/chapter_9/p_276/src/HorrorShow.java
@@ -0,0 +1,62 @@
+interface Monster {
+    void menace();
+}
+
+interface DangerousMonster extends Monster {
+    void destroy();
+}
+
+interface Lethal {
+    void kill();
+}
+
+class DragonZilla implements DangerousMonster {
+    public void menace() {}
+
+    @Override
+    public void destroy() {}
+}
+
+interface Vampire extends DangerousMonster, Lethal {
+    void drinkBlood();
+}
+
+class VerBadVampire implements Vampire {
+    @Override
+    public void menace() {}
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public void kill() {}
+
+    @Override
+    public void drinkBlood() {}
+}
+
+public class HorrorShow {
+    static void u(Monster b) {
+        b.menace();
+    }
+
+    static void v(DangerousMonster d){
+        d.menace();
+        d.destroy();
+    }
+
+    static void w(Lethal l) {
+        l.kill();
+    }
+
+    public static void main(String[] args) {
+        DangerousMonster barney=new DragonZilla();
+        u(barney);
+        v(barney);
+
+        Vampire vlad=new VerBadVampire();
+        u(vlad);
+        v(vlad);
+        w(vlad);
+    }
+}
