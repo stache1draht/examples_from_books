8e35a362b458d980ad4c525464ec91cbb21f8a1d
grikulik <grigory.kulik@t-systems.com>
2021-02-20 17:04:16 +0300

    no message

---
+++ java/kay_horstman/v_1/ch_3/src/Main.java
@@ -0,0 +1,35 @@
+public class Main
+    {
+        public static void main(String[] args) {
+            /**
+             * If you need to join two strings delimited with a symbol, you can join them
+             * using the join() method.
+             */
+            String all=String.join("/", "L", "M", "S");
+            System.out.println(all);
+
+
+            /**
+             * To determin the real length of a string, use the following method.
+             */
+            String greeting="Oello";
+            int cpCount=greeting.codePointCount(0, greeting.length());
+            System.out.println(cpCount);
+
+
+            /**
+             * The codePoints() method returns a stream of int values for separate code points.
+             */
+            int[] codePoints=greeting.codePoints().toArray();
+            for (int i:codePoints) {
+                System.out.println(i);
+            }
+
+
+            /**
+             * An array of code points can be converted to a string using a String constructor,
+             * as shown below.
+             */
+            String str=new String(codePoints, 0, codePoints.length);
+        }
+    }
